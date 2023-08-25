3806d2488524a962eddd5fab75ffe3768cfe783d
Laura Leite <111168549+Lauragpse@users.noreply.github.com>
2023-08-25 20:00:13 -0300

    Create listasConstantes.py

---
+++ listasConstantes.py
@@ -0,0 +1,19 @@
+letra = str(input())
+v = ['a', 'e', 'i', 'o', 'u']
+p = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+n = [10.0, 9.5, 8.0, 7.5]
+a = ['Maria', 85, 9.5]
+z = []
+
+if letra == 'v' or letra == 'V':
+    print(v)
+elif letra == 'p' or letra == 'P':
+    print(p)
+elif letra == 'n' or letra == 'N':
+    print(n)
+elif letra == 'a' or letra =='A':
+    print(a)
+elif letra == 'z' or letra == 'Z':
+    print(z)
+else:
+    print('')
